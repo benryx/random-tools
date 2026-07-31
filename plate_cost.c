79119a16704645bd1d4197408309b1e559f12c7c
benryx <bhenry97@gmail.com>
2026-07-31 20:08:23 +0000

    Fix up plate_cost

--- plate_cost.c
+++ plate_cost.c
@@ -9,29 +9,30 @@ int main(int argc, char *argv[]) {
 
         double dollars_per_pound = 1.75;
 
-        double thickness;
-        double length;
-        double width;
+        double a;
+        double b;
+        double c;
 
         double weight;
         double cost;
         double hours;
 
         if (argc == 4) {
-                thickness = strtod(argv[1], NULL);
-                length = strtod(argv[2], NULL);
-                width = strtod(argv[3], NULL);
+                a = strtod(argv[1], NULL);
+                b = strtod(argv[2], NULL);
+                c = strtod(argv[3], NULL);
         } else {
-                fprintf(stderr, "Enter plate dimensions (T L W): ");
-                scanf("%lf%lf%lf", &thickness, &length, &width);
+                fprintf(stderr, "Enter plate dimensions (space-separated): ");
+                scanf("%lf%lf%lf", &a, &b, &c);
         }
 
-        weight = density * thickness * length * width;
+        weight = density * a * b * c;
         cost = weight * dollars_per_pound;
         hours = cost / HOURLY_RATE;
 
         printf("\n%.2f lbs\n", weight);
-        printf("Cost = $%.2f (%.2f hrs)\n\n", cost, hours);
+        printf("Cost = $%.2f [%.0fh %dm]\n\n",
+               cost, hours, (int)(hours * 60) % 60);
 
         exit(EXIT_SUCCESS);
 }
