29a7c26d2b141fcdf63c84f312149c233932be27
benryx <bhenry97@gmail.com>
2026-08-03 20:47:21 +0000

    Revamp encode.c, but currently broken!

--- encode.c
+++ encode.c
@@ -6,30 +6,24 @@
 #define LEXICON_FILENAME ".lexicon"
 #define MAX_FILEPATH_SIZE 64
 #define MAX_LEXICON_SIZE 0xFFFFF
-#define MAX_WORDS 64
-#define MAX_WORD_SIZE 64
+#define MAX_TOKENS 64
+#define MAX_TOKEN_SIZE 64
 #define MAX_BIN_WIDTH 20
-#define MAX_RESULT_SIZE 32
 
-char lexicon[MAX_LEXICON_SIZE][MAX_WORD_SIZE + 1];
-char line[MAX_WORDS * (MAX_WORD_SIZE + 1)];
-char *words[MAX_WORDS];
-size_t indices[MAX_WORDS];
-char binary[MAX_WORDS][MAX_BIN_WIDTH + 1];
-char combined[MAX_WORDS * MAX_BIN_WIDTH + 1];
-char result[MAX_RESULT_SIZE];
-unsigned char bytes[MAX_WORDS * MAX_BIN_WIDTH / 8];
+char lexicon[MAX_LEXICON_SIZE][MAX_TOKEN_SIZE + 1];
+char line[MAX_TOKENS * (MAX_TOKEN_SIZE + 1)];
+char *tokens[MAX_TOKENS];
+unsigned long indices[MAX_TOKENS];
+unsigned char bytes[MAX_TOKENS * MAX_BIN_WIDTH / 8];
 
 void encode(void);
 void decode(void);
 int read_lexicon(void);
-int read_words(void);
-size_t find_indices(int n_words);
+int read_tokens(void);
+unsigned long find_indices(int n_tokens);
+void print_binary(unsigned long number);
+int interleave(int n_tokens);
 int save_lexicon(void);
-void fill_binary(int n_words, int width);
-int bit_width(size_t number);
-void interleave(int n_words, int width);
-int result_to_bytes(void);
 
 int main(void) {
         /*int choice;
@@ -55,9 +49,7 @@ int main(void) {
 }
 
 void encode(void) {
-        int n_words;
-        size_t max_index;
-        int max_width;
+        int n_tokens;
         int i;
         int n_bytes;
 
@@ -65,32 +57,23 @@ void encode(void) {
                 fprintf(stderr, "Failed to read lexicon from file.\n");
         }
 
-        n_words = read_words();
+        n_tokens = read_tokens();
 
-        printf("Tokens: %d\n", n_words);
+        printf("Tokens: %d\n", n_tokens);
 
-        max_index = find_indices(n_words);
-        max_width = bit_width(max_index);
-        printf("Max Bit Width: %d\n", max_width);
+        find_indices(n_tokens);
 
-        fill_binary(n_words, max_width);
-
-        for (i = 0; i < n_words; i++) {
-                printf("[%3ld,%3lx] %s: %s\n",
+        for (i = 0; i < n_tokens; i++) {
+                print_binary(indices[i]);
+                printf(" [%3ld,%3lx]: %s\n",
                        indices[i],
                        indices[i],
-                       binary[i],
                        lexicon[indices[i]]
                 );
         }
 
-        interleave(n_words, max_width);
+        n_bytes = interleave(n_tokens);
 
-        printf("Combined binary:\n\t%s\n", combined);
-
-        n_bytes = result_to_bytes();
-
-        printf("Final result:\n\t");
         for (i = 0; i < n_bytes; i++) {
                 printf("%02x ", bytes[i]);
         }
@@ -121,7 +104,7 @@ int read_lexicon() {
         }
 
         for (i = 0; (fgets(lexicon[i],
-                           (MAX_WORD_SIZE + 1) * sizeof(char), fp)) != NULL; i++) {
+                           (MAX_TOKEN_SIZE + 1) * sizeof(char), fp)) != NULL; i++) {
                 lp = lexicon[i];
 
                 /* We don't want blank entries, overwrite these. */
@@ -142,9 +125,9 @@ int read_lexicon() {
         return 1;
 }
 
-int read_words() {
+int read_tokens() {
         char *p;
-        int n_words = 0;
+        int n_tokens = 0;
 
         if ((fgets(line, sizeof(line), stdin)) != NULL) {
                 p = line;
@@ -164,8 +147,8 @@ int read_words() {
                                 break;
                         }
 
-                        /* Otherwise, we are at the start of a word */
-                        words[n_words++] = p;
+                        /* Otherwise, we are at the start of a token */
+                        tokens[n_tokens++] = p;
 
                         /* Skip until whitespace or null */
                         while (
@@ -182,25 +165,25 @@ int read_words() {
                                 break;
                         }
 
-                        /* Otherwise, mark end of this word, and advance */
+                        /* Otherwise, mark end of this token, and advance */
                         *p++ = '\0';
                 }
         }
 
-        return n_words;
+        return n_tokens;
 }
 
-size_t find_indices(int n_words) {
+unsigned long find_indices(int n_tokens) {
+        unsigned long j;
+        unsigned long max_index = 0;
         int i;
-        size_t j;
-        size_t max_index = 0;
 
-        for (i = 0; i < n_words; i++) {
+        for (i = 0; i < n_tokens; i++) {
                 for (j = 0; j < MAX_LEXICON_SIZE; j++) {
                         if (*lexicon[j] == '\0') {
-                                strcpy(lexicon[j], words[i]);
+                                strcpy(lexicon[j], tokens[i]);
                                 break;
-                        } else if (strcmp(words[i], lexicon[j]) == 0) {
+                        } else if (strcmp(tokens[i], lexicon[j]) == 0) {
                                 break;
                         }
                 }
@@ -213,6 +196,47 @@ size_t find_indices(int n_words) {
         }
 
         return max_index;
+}
+
+void print_binary(unsigned long number) {
+        if (!number) {
+                putchar('0');
+        }
+
+        while (number) {
+                putchar((number & 1) + '0');
+                number >>= 1;
+        }
+
+        return;
+}
+
+int interleave(int n_tokens) {
+        unsigned char *bp = bytes;
+        int i = 0;
+        int n_bits = 0;
+        int remaining = n_tokens;
+        int shift = 0;
+
+        while (remaining) {
+                if (!(indices[i] >> shift)) {
+                        remaining--;
+                }
+
+                *bp = (*bp << 1) | ((indices[i] >> shift) & 1);
+
+                if (++n_bits == 8) {
+                        bp++;
+                        n_bits = 0;
+                }
+
+                if (++i == n_tokens) {
+                        i = 0;
+                        shift++;
+                }
+        }
+
+        return (bp - bytes) + 1;
 }
 
 int save_lexicon(void) {
@@ -234,78 +258,4 @@ int save_lexicon(void) {
         fclose(fp);
 
         return 1;
-}
-
-void fill_binary(int n_words, int width) {
-        size_t number;
-        int i;
-        int j;
-
-        for (i = 0; i < n_words; i++) {
-                number = indices[i];
-
-                binary[i][width] = '\0';
-                for (j = width - 1; j >= 0; j--) {
-                        binary[i][j] = (number & 1) + '0';
-                        number >>= 1;
-                }
-        }
-
-        return;
-}
-
-int bit_width(size_t number) {
-        int width;
-
-        for (width = 0; number != 0; width++) {
-                number >>= 1;
-        }
-
-        return width;
-}
-
-void interleave(int n_words, int width) {
-        char *cp = combined;
-        int x;
-        int r;
-        int c;
-
-        /*
-         * Pad with zeros in the front until it's a multiple of 8.
-         * So then we can split it up into bytes.
-         */
-        x = n_words * width;
-        while (x++ % 8 != 0) {
-                *cp++ = '0';
-        }
-
-        for (c = 0; c < width; c++) {
-                for (r = 0; r < n_words; r++) {
-                        *cp++ = binary[r][c];
-                }
-        }
-
-        *cp = '\0';
-}
-
-int result_to_bytes(void) {
-        char *cp = combined;
-        unsigned char byte;
-        int i;
-        int n_bytes = 0;
-
-        while (*cp != '\0') {
-                byte = 0;
-
-                for (i = 0; i < 8; i++) {
-                        if (*cp == '\0') {
-                                break;
-                        }
-                        byte = (byte << 1) | (*cp++ - '0');
-                }
-
-                bytes[n_bytes++] = byte;
-        }
-
-        return n_bytes;
 }
